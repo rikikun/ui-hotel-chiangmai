77efb815d8ad2b7a28aa38e0ffc7752fdcdb5e74
Benyapa Chayakornwatchara <benyapa.cha@ascendcorp.com>
2018-04-16 21:19:20 +0700

    Add action add and remove hotel list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HOTELS } from './mock-hotel';
+import { Hotel } from './entity/hotel';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,20 @@ export class AppComponent implements OnInit{
 
   hotels = HOTELS;
 
+  hotel: Hotel = {
+    name: '',
+    number: 0,
+    bedInfo: ''
+  };
+
   ngOnInit() {
+  }
+
+  onAdd(): void {
+    this.hotels.push(this.hotel);
+  }
+
+  onRemove(index: number): void {
+    this.hotels = this.hotels.filter((value, i) => index !== i);
   }
 }
